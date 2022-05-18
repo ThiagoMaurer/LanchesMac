e1c27df47ba0acf4a456bb566fcc88e5248bf46b
Thiago Augusto Maurer <thiagomaurer.mdp@gmail.com>
2022-05-17 23:44:06 -0300

    fix - Model Categoria

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -9,16 +9,15 @@ public class Categoria
     [Key]
     public int CategoriaId { get; set; }
 
-    [StringLength(100, ErrorMessage = "O tamanho máximo é 100 caracteres.")]
-    [Required(ErrorMessage = "Informe o nome da categoria")]
-    [Display(Name = "Nome")]
+    [Display(Name = "O nome da categoria")]
+    [Required(ErrorMessage = "{0} deve ser informado.")]
+    [MaxLength(100, ErrorMessage = "{0} deve ter no máximo {1} caracteres.")]
     public string Nome { get; set; }
 
-    [StringLength(200, ErrorMessage = "O tamanho máximo é 100 caracteres.")]
-    [Required(ErrorMessage = "Informe a descrição da categoria")]
-    [Display(Name = "Descricao")]
+    [Display(Name = "A descrição da categoria")]
+    [Required(ErrorMessage = "{0} deve ser informada.")]
+    [MaxLength(200, ErrorMessage = "{0} deve ter no máximo {1} caracteres.")]
     public string Descricao { get; set; }
-
 
     //Relacionamento Categoria-Lanche(s) (1 para muitos)
     public List<Lanche> Lanches { get; set; }
